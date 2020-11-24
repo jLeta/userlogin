5fe2e12285c55e7e9552bdcea6f8e86e1ee08326
ljifar <leta@Letas-MBP.fios-router.home>
2020-11-23 21:37:57 -0500

    fix build script

--- build-and-start-app.sh
+++
@@ -1,7 +0,0 @@
-
-echo 'Preparing frontend build...'
-cd userlogin-frontend && yarn install && yarn build
-echo 'Preparing backend build and archiving jar containing both frontend and backend...'
-cd ../userlogin-backend && mvn clean install
-echo 'lauching the application ...'
-java -jar target/userlogin-0.0.1-SNAPSHOT.jar
